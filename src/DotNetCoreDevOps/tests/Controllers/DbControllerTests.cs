d98a5678a4c8b9e806ae694b4466c755bc59259a
Khurram Nizami <ceo@medrender.com>
2017-02-14 21:08:44 -0600

    added integration test dependency

--- src/DotNetCoreDevOps/tests/Controllers/DbControllerTests.cs
+++ src/DotNetCoreDevOps/tests/Controllers/DbControllerTests.cs
@@ -5,29 +5,57 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using CloudantDotNet.Services;
 using CloudantDotNet.Models;
+using CloudantDotNet.Controllers;
 using Moq;
+using Newtonsoft.Json;
 
 namespace TestControllers.Tests.UnitTests
 {
     public class DbControllerTests
     {
+        DbController _dbcontroller;
+        Auth[] ValidUsers = new Auth[] {
+            new Auth { username = "user1", password = "password" },
+            new Auth { username = "knizami", password = "passw0rd" }
+        };
+
+        Auth[] InvalidUsers = new Auth[] {
+
+        };
+
+        public DbControllerTests()
+        {
+        }
+
+
         [Fact]
-        public async Task Payees_ReturnsAJSONResult_WithAListOfPayees()
+        public async Task Valid_Login_Should_Return_JSON_Success()
         {
-            /*var MockDbSvc = new Mock<ICloudantService>();
-            MockDbSvc.Setup(svc => svc.GetPayeesAsync(It.IsAny<Auth>()))
-            .Returns(Task.FromResult(GetPayees()));
-            */
+            var MockDbSvc = new Mock<ICloudantService>();
+            MockDbSvc.Setup(svc => svc.LoginAsync(It.Is<Auth>(u => u.username == "knizami")))
+            .ReturnsAsync(Task.FromResult(GetSuccessLogin()));
+            _dbcontroller = new DbController(MockDbSvc.Object);
+            Auth userlogin = new Auth()
+            {
+                username = "knizami",
+                password = "passw0rd"
+            };
+
+            var result = await _dbcontroller.Login(userlogin);
+            Console.WriteLine(result);
+            var loginResult = Assert.IsType<string>(result);
+            Assert.IsType<AuthResult>(JsonConvert.DeserializeObject<AuthResult>(result));
+
+
 
         }
 
-        private async Task<dynamic> GetPayees()
+        private async Task<dynamic> GetSuccessLogin()
         {
-            CheckResult checks = new CheckResult();
-
-            return checks;
+            return JsonConvert.SerializeObject(new AuthResult()
+            {
+                status = "success"
+            });
         }
-
-
     }
 }
